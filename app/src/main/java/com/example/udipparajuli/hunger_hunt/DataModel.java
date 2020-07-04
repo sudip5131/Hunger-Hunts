97a3529b8f737326047713603c73f8b2215b8bc5
Sudip Maharjan <messudip19@gmail.com>
2020-07-04 15:21:01 +0545

    yo

---
+++ app/src/main/java/com/example/udipparajuli/hunger_hunt/DataModel.java
@@ -0,0 +1,14 @@
+package com.example.udipparajuli.hunger_hunt;
+
+public class DataModel {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+
+}
